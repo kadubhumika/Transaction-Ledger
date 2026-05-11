c63f47603cbda1925d6ea751026532faf333cdf2
BHUMIKA KADU✨ <kadubhumika2468@gmail.com>
2026-05-11 21:22:54 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
-# main.[y
-#run app
 from fastapi import FastAPI
+from fastapi import WebSocket
 
-from backend import *
+from fastapi.middleware.cors import CORSMiddleware
 
-from backend import *
-
-from backend import *
 from backend.database import Base, engine
 from backend.routes import router
-from fastapi import FastAPI, WebSocket
 from backend.websocket_manager import manager
+
+
+# create postgres tables
 
 Base.metadata.create_all(bind=engine)
 
+
+# fastapi app
+
 app = FastAPI()
 
+
+# cors
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# api routes
+
 app.include_router(router)
+
+
+# websocket route
+
 @app.websocket("/ws/{email}")
 
 async def websocket_endpoint(
